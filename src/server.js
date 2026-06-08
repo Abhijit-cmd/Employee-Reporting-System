c045408416fb1c10dee3c58f3bae801d9e83c679
Kaustav-JB <kaustavjbora@gmail.com>
2026-06-08 20:22:23 +0530

    Fixed version mismatch for path-to-regexp

--- src/server.js
+++ src/server.js
@@ -91,7 +91,7 @@ app.use("/api", routes);
 if (process.env.NODE_ENV === "production") {
   const clientDist = path.join(__dirname, "..", "client", "dist");
   app.use(express.static(clientDist));
-  app.get("*", (_req, res) => {
+  app.get("/*splat", (_req, res) => {
     res.sendFile(path.join(clientDist, "index.html"));
   });
 }
